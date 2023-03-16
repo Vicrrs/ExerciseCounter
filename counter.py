6e2afee5578d596caa7ec83b52f040f7e57c3dd2
Vicrrs <enricofermi2019@gmail.com>
2023-03-16 03:18:48 -0400

    displaying frames

--- counter.py
+++ counter.py
@@ -4,3 +4,10 @@ import mediapipe as mp
 
 # Taking the path of the video
 video = cv2.VideoCapture()
+
+while True:
+    success, img = video.read()
+    videoRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    cv2.imshow('videoRGB', img)
+    cv2.waitKey(0)
